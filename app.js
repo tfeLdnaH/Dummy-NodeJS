c4945edfe33a0ce40fb188b7887414491c44073e
Kodomo <anderson.morita@gmail.com>
2018-06-11 21:52:06 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,158 +1,25 @@
-var http = require('http'),
-    fs = require('fs');
+/**
+ * Created by Anderson on 01/07/2017.
+ */
+function placeAnOrder(orderNumber) {
+    console.log("Customer order:", orderNumber);
 
-function load_album_list(callback) {
-    // we will just assume that any directory in our 'albums'
-    // subfolder is an album.
-    fs.readdir("albums", (err, files) => {
-        if (err) {
-            callback(make_error("file_error",  JSON.stringify(err)));
-            return;
-        }
-
-        var only_dirs = [];
-
-        var iterator = (index) => {
-            if (index == files.length) {
-                callback(null, only_dirs);
-                return;
-            }
-
-            fs.stat("albums/" + files[index], (err, stats) => {
-                if (err) {
-                    callback(make_error("file_error",
-                                        JSON.stringify(err)));
-                    return;
-                }
-                if (stats.isDirectory()) {
-                    var obj = { name: files[index] };
-                    only_dirs.push(obj);
-                }
-                iterator(index + 1)
-            });
-
-        }
-        iterator(0);
+    cookDeliveryFood( function () {
+        console.log("delivery food order:", orderNumber);
     });
 }
 
-function load_album(album_name, callback) {
-    // we will just assume that any directory in our 'albums'
-    // subfolder is an album.
-    fs.readdir("albums/" + album_name, (err, files) => {
-        if (err) {
-            if (err.code == "ENOENT") {
-                callback(no_such_album());
-            } else {
-                callback(make_error("file_error",
-                                    JSON.stringify(err)));
-            }
-            return;
-        }
+//simulate a 5 second operation
+function cookDeliveryFood(callback) {
+    setTimeout(callback, 5000);  //não precisa esperar os 5 ssegundos, meio que diz para o sistema vai fazer o q vc tem q fazer e depois de 5 segundos me executa
 
-        var only_files = [];
-        var path = `albums/${album_name}/`;
-
-        var iterator = (index) => {
-            if (index == files.length) {
-                var obj = { short_name: album_name,
-                            photos: only_files };
-                callback(null, obj);
-                return;
-            }
-
-            fs.stat(path + files[index], (err, stats) => {
-                if (err) {
-                    callback(make_error("file_error",
-                                        JSON.stringify(err)));
-                    return;
-                }
-                if (stats.isFile()) {
-                    var obj = { filename: files[index],
-                                desc: files[index] };
-                    only_files.push(obj);
-                }
-                iterator(index + 1)
-            });
-        }
-        iterator(0);
-    });
 }
 
-function handle_incoming_request(req, res) {
-    console.log("INCOMING REQUEST: " + req.method + " " + req.url);
-    if (req.url == '/albums.json') {
-        handle_list_albums(req, res);
-    } else if (req.url.substr(0, 7) == '/albums'
-               && req.url.substr(req.url.length - 5) == '.json') {
-        handle_get_album(req, res);
-    } else {
-        send_failure(res, 404, invalid_resource());
-    }
-}
+//simulate user webrequest
 
-function handle_list_albums(req, res) {
-    load_album_list((err, albums) => {
-        if (err) {
-            send_failure(res, 500, err);
-            return;
-        }
-
-        send_success(res, { albums: albums });
-    });
-}
-
-function handle_get_album(req, res) {
-    // format of request is /albums/album_name.json
-    var album_name = req.url.substr(7, req.url.length - 12);
-    load_album(album_name, (err, album_contents) => {
-        if (err && err.error == "no_such_album") {
-            send_failure(res, 404, err);
-        }  else if (err) {
-            send_failure(res, 500, err);
-        } else {
-            send_success(res, { album_data: album_contents });
-        }
-    });
-}
-
-function make_error(err, msg) {
-    var e = new Error(msg);
-    e.code = err;
-    return e;
-}
-
-function send_success(res, data) {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    var output = { error: null, data: data };
-    res.end(JSON.stringify(output) + "\n");
-}
-
-function send_failure(res, server_code, err) {
-    var code = (err.code) ? err.code : err.name;
-    res.writeHead(server_code, { "Content-Type" : "application/json" });
-    res.end(JSON.stringify({ error: code, message: err.message }) + "\n");
-}
-
-
-function invalid_resource() {
-    return make_error("invalid_resource",
-                      "the requested resource does not exist.");
-}
-
-function no_such_album() {
-    return make_error("no_such_album",
-                      "The specified album does not exist");
-}
-
-var s = http.createServer(handle_incoming_request);
-s.listen(8080);
-/*the result
-C:\Users\Anderson\Desktop\PytonGitUI5JSAngularETC\NodeJS\src>curl -X GET http://localhost:8080/albums.json
-{"error":null,"data":{"albums":[{"name":"australia"},{"name":"italy"},{"name":"japan"}]}}
-
-C:\Users\Anderson\Desktop\PytonGitUI5JSAngularETC\NodeJS\src>curl -X GET http://localhost:8080/albums/japan.json
-{"error":null,"data":{"album_data":{"short_name":"/japan","photos":[{"filename":"slide-1.jpg","desc":"slide-1.jpg"},{"filename":"slide-2.jpg","desc":"slide-2.jpg"},{"filename":"slide-3.jpg","desc":"slide-3.jpg"}]}}}
-
-
-*/
+placeAnOrder(1);
+placeAnOrder(2);
+placeAnOrder(3);
+placeAnOrder(4);
+placeAnOrder(5);
+placeAnOrder(6);
